8d0154a92bc88269570200d4d12e2519b2701b31
Marc Chan <tonychan8201@gmail.com>
2026-07-03 06:01:01 +0000

    add instruction set

--- include/instruction.h
+++ include/instruction.h
@@ -10,14 +10,14 @@ typedef void (*InstructionHandler)(Chip8* chip8, uint16_t opcode);
 void instruction_execute(Chip8 *chip8, uint16_t opcode);
 
 void handle_0000(Chip8 *chip8, uint16_t opcode);
-void handle_jump(Chip8 *Chip8, uint16_t opcode);
-void handle_call(Chip8 *Chip8, uint16_t Opcode); // Handles 2NNN
-// void handle_se_vx_nn(Chip8 *Chip8, uint16_t Opcode); // Handles 3XNN
-// void handle_sne_vx_nn(SChip8 *Chip8, uint16_t Opcode); // Handles 4XNN
-// void handle_se_vx_vy(SChip8 *Chip8, uint16_t Opcode); // Handles 5XY0
-// void handle_ld_vx_nn(SChip8 *Chip8, uint16_t Opcode); // Handles 6XNN
-// void handle_add_vx_nn(SChip8 *Chip8, uint16_t Opcode); // Handles 7XNN
-// void handle_8000(SChip8 *Chip8, uint16_t Opcode); // Sub-switch for arithmetic group 8XY0 - 8XYE
+void handle_jump(Chip8 *chip8, uint16_t opcode);
+void handle_call(Chip8 *chip8, uint16_t opcode); // Handles 2NNN
+void handle_se_vx_nn(Chip8 *chip8, uint16_t opcode); // Handles 3XNN
+void handle_sne_vx_nn(Chip8 *chip8, uint16_t opcode); // Handles 4XNN
+void handle_se_vx_vy(Chip8 *chip8, uint16_t opcode); // Handles 5XY0
+void handle_ld_vx_nn(Chip8 *chip8, uint16_t opcode); // Handles 6XNN
+void handle_add_vx_nn(Chip8 *chip8, uint16_t opcode); // Handles 7XNN
+void handle_8000(Chip8 *chip8, uint16_t opcode); // Sub-switch for arithmetic group 8XY0 - 8XYE
 // void handle_sne_vx_vy(SChip8 *Chip8, uint16_t Opcode); // Handles 9XY0
 // void handle_ld_i_nnn(SChip8 *Chip8, uint16_t Opcode); // Handles ANNN
 // void handle_jp_v0_nnn(SChip8 *Chip8, uint16_t Opcode); // Handles BNNN
--- lib/instruction.c
+++ lib/instruction.c
@@ -2,12 +2,32 @@
 #include "common.h"
 #include <stdio.h>
 
-const InstructionHandler instruction_table[] = {[0x0] = handle_0000, [0x1] = handle_jump, [0x2] = handle_call};
+const InstructionHandler instruction_table[] = {[0x0] = handle_0000,
+                                                [0x1] = handle_jump,
+                                                [0x2] = handle_call,
+                                                [0x3] = handle_se_vx_nn,
+                                                [0x4] = handle_sne_vx_nn,
+                                                [0x5] = handle_se_vx_vy,
+                                                [0x6] = handle_ld_vx_nn,
+                                                [0x7] = handle_add_vx_nn,
+                                                [0x8] = handle_8000};
 
 typedef enum {
     OP_CLS = 0x00E0, // Clear the display.
     OP_RET = 0x00EE  // Return from a subroutine
-} Opcode;
+} Opcode_0_Subtype;
+
+typedef enum {
+    OP_LD = 0x0,
+    OP_OR = 0x1,
+    OP_AND = 0x2,
+    OP_XOR = 0x3,
+    OP_ADD = 0x4,
+    OP_SUB = 0x5,
+    OP_SHR = 0X6,
+    OP_SUBN = 0x7,
+    OP_SHL = 0XE
+} Opcode_8_Subtype;
 
 void instruction_execute(Chip8 *chip8, uint16_t opcode) {
     uint8_t idx = NIBBLE_TO_INDEX(opcode & 0xF000);
@@ -48,5 +68,116 @@ void handle_call(Chip8 *chip8, uint16_t opcode) {
     chip8->pc = opcode & 0x0FFF;
 }
 
+void handle_se_vx_nn(Chip8 *chip8, uint16_t opcode) {
+    // Register index
+    uint8_t x = (opcode & 0x0F00 >> 8);
+    uint8_t nn = opcode & 0x00FF;
+    if (chip8->V[x] == nn) {
+        chip8->pc += 4;
+    } else {
+        chip8->pc += 2;
+    }
+}
 
+void handle_sne_vx_nn(Chip8 *chip8, uint16_t opcode) {
+    uint8_t x = (opcode & 0x0F00) >> 8;
+    uint8_t nn = opcode & 0x00FF;
+    if (chip8->V[x] != nn) {
+        chip8->pc += 4;
+    } else {
+        chip8->pc += 2;
+    }
+}
 
+void handle_se_vx_vy(Chip8 *chip8, uint16_t opcode) {
+    uint8_t x = (opcode & 0x0F00) >> 8;
+    uint8_t y = (opcode & 0x00F0) >> 4;
+    if (chip8->V[x] == chip8->V[y]) {
+        chip8->pc += 4;
+    } else {
+        chip8->pc += 2;
+    }
+}
+
+void handle_ld_vx_nn(Chip8 *chip8, uint16_t opcode) {
+    uint8_t x = (opcode & 0x0F00) >> 8;
+    uint8_t kk = (opcode & 0x00FF);
+    chip8->V[x] = kk;
+    chip8->pc += 2;
+}
+
+void handle_add_vx_nn(Chip8 *chip8, uint16_t opcode) {
+    uint8_t x = (opcode & 0x0F00) >> 8;
+    uint8_t kk = (opcode & 0x00FF);
+    chip8->V[x] += kk;
+    chip8->pc += 2;
+}
+
+void handle_8000(Chip8 *chip8, uint16_t opcode) {
+    uint8_t x = (opcode & 0x0F00) >> 8;
+    uint8_t y = (opcode & 0x00F0) >> 4;
+    uint8_t type = opcode & 0x000F;
+    switch (type) {
+    case OP_LD:
+        chip8->V[x] = chip8->V[y];
+        break;
+    case OP_OR:
+        chip8->V[x] |= chip8->V[y];
+        break;
+    case OP_AND:
+        chip8->V[x] &= chip8->V[y];
+        break;
+    case OP_XOR:
+        chip8->V[x] ^= chip8->V[y];
+        break;
+    case OP_ADD:
+        bool carry = chip8->V[x] > (0xFF - chip8->V[y]);
+        if (carry) {
+            chip8->V[15] = 1;
+        } else {
+            chip8->V[15] = 0;
+        }
+        chip8->V[x] += chip8->V[y];
+        break;
+    case OP_SUB:
+        bool borrow = chip8->V[x] > chip8->V[y];
+        if (borrow) {
+            chip8->V[15] = 1;
+        } else {
+            chip8->V[15] = 0;
+        }
+        chip8->V[x] -= chip8->V[y];
+    case OP_SHR:
+        uint8_t lsb = chip8->V[x] & 0x0001;
+        if (lsb == 1) {
+            chip8->V[15] = 1;
+        } else {
+            chip8->V[15] = 0;
+        }
+        chip8->V[x] = chip8->V[x] >> 1;
+        break;
+    case OP_SUBN:
+        bool borrow = chip8->V[y] > chip8->V[x];
+        if (borrow) {
+            chip8->V[15] = 1;
+        } else {
+            chip8->V[15] = 0;
+        }
+        chip8->V[x] = chip8->V[y] - chip8->V[x];
+        break;
+    case OP_SHL:
+        uint8_t msb = chip8->V[x] >> 7 & 0x01;
+        if (msb == 1) {
+            chip8->V[15] = 1;
+        } else {
+            chip8->V[15] = 0;
+        }
+        chip8->V[x] = chip8->V[x] << 1;
+        break;
+    default:
+        fprintf(stderr, "Unknown opcode %d", opcode);
+        break;
+    }
+
+    chip8->pc += 2;
+}
